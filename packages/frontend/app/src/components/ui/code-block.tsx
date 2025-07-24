f32c4fe4489a31280f7488b024470d022f681986
Cats Juice <wp1298554944@hotmail.com>
2025-07-24 23:44:01 +0800

    feat: add copy to code block

--- packages/frontend/app/src/components/ui/code-block.tsx
+++ packages/frontend/app/src/components/ui/code-block.tsx
@@ -1,7 +1,11 @@
-import { IconButton } from '@afk/component';
-import { ExpandCloseIcon, ExpandFullIcon } from '@blocksuite/icons/rc';
+import { IconButton, observeResize, toast } from '@afk/component';
+import {
+  CopyIcon,
+  ExpandCloseIcon,
+  ExpandFullIcon,
+} from '@blocksuite/icons/rc';
 import { cssVarV2 } from '@toeverything/theme/v2';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { useHighlightedCode } from '../../lib/hooks/use-highlighted-code';
 
@@ -13,8 +17,19 @@ export const CodeBlock = ({
   language: string;
 }) => {
   const [expanded, setExpanded] = useState(false);
+  const [hitMaxHeight, setHitMaxHeight] = useState(false);
+  const codeBlockRef = useRef<HTMLDivElement>(null);
 
   const html = useHighlightedCode(children as string, language);
+
+  useEffect(() => {
+    if (!codeBlockRef.current) return;
+    return observeResize(codeBlockRef.current, () => {
+      if (!codeBlockRef.current) return;
+      const height = codeBlockRef.current.scrollHeight;
+      setHitMaxHeight(height > 400);
+    });
+  }, []);
 
   return (
     <div className="custom-code-block w-full rounded-xl border not-prose overflow-hidden">
@@ -23,12 +38,24 @@ export const CodeBlock = ({
         style={{ backgroundColor: cssVarV2('toast/overlay/secondary') }}
       >
         <div className="text-sm text-text-secondary ">{language}</div>
-        <IconButton
-          icon={expanded ? <ExpandCloseIcon /> : <ExpandFullIcon />}
-          onClick={() => setExpanded(!expanded)}
-        />
+        <div className="flex items-center gap-2">
+          <IconButton
+            icon={<CopyIcon />}
+            onClick={() => {
+              navigator.clipboard.writeText(children as string);
+              toast('Copied to clipboard');
+            }}
+          />
+          {hitMaxHeight ? (
+            <IconButton
+              icon={expanded ? <ExpandCloseIcon /> : <ExpandFullIcon />}
+              onClick={() => setExpanded(!expanded)}
+            />
+          ) : null}
+        </div>
       </header>
       <div
+        ref={codeBlockRef}
         className="p-4 text-[13px] overflow-auto"
         dangerouslySetInnerHTML={{ __html: html }}
         style={{
